b4df109d11f3023516e8fb148b7c12d89e33baac
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-02-09 18:51:50 +0600

    Added customer and order model

---
+++ DashboardBackend/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace DashboardBackend.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        Pending,
+        Paused,
+        Processing,
+        Delivered,
+    }
+}
