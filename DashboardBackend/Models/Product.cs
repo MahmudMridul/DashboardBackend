1394b65338fc6bb3cb7475497396a55ca399f153
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-02-09 21:06:14 +0600

    Add product model

---
+++ DashboardBackend/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿namespace DashboardBackend.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public Category Category { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public ProductStatus Status { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+
+    public enum Category
+    {
+        Men,
+        Women, 
+        Children,
+        Sports,
+        Graphic
+    }
+
+    public enum Size
+    {
+        Small,
+        Medium,
+        Large,
+        XLarge,
+        XXLarge
+    }
+
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Black, 
+        White, 
+    }
+
+    public enum ProductStatus
+    {
+        Low,
+        Medium,
+        Ok
+    }
+
+}
