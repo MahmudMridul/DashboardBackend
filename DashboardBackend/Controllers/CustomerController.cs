731d9c832f8f4bfb4c42d4de34f1ae6300393d7c
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-02-12 21:43:53 +0600

    Add customer controller

---
+++ DashboardBackend/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using DashboardBackend.Data;
+using DashboardBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DashboardBackend.Controllers
+{
+    [Route("api/customer")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationContext _db;
+
+        public CustomerController(ApplicationContext db) 
+        {
+            _db = db;
+        }
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _db.Customers.ToListAsync();
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
