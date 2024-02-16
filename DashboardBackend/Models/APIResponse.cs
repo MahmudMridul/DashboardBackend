8a2ef6602a7b56b88faa1535a2be352773e37c8d
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-02-16 14:47:22 +0600

    Add api response model

---
+++ DashboardBackend/Models/APIResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace DashboardBackend.Models
+{
+    public class APIResponse
+    {
+        public object? Data { get; set; } = null;
+        public bool IsSuccess { get; set; } = false;
+        public HttpStatusCode StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
