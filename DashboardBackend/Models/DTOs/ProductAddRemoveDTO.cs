a6a9d15eca5468c45e7b21dd180219b2b6edfcd8
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-03-04 09:19:45 +0600

    Add ProductAddRemoveDTO

---
+++ DashboardBackend/Models/DTOs/ProductAddRemoveDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DashboardBackend.Models.DTOs
+{
+    public class ProductAddRemoveDTO
+    {
+        public Category Category { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ DashboardBackend/Models/Enums.cs
@@ -0,0 +1,37 @@
+﻿public enum Category
+{
+    All,
+    Men,
+    Women,
+    Children,
+    Sports,
+    Graphic, 
+}
+
+public enum Size
+{
+    All,
+    Small,
+    Medium,
+    Large,
+    XLarge,
+    XXLarge
+}
+
+public enum Color
+{
+    All,
+    Red,
+    Blue,
+    Green,
+    Yellow,
+    Black,
+    White,
+}
+
+public enum ProductStatus
+{
+    Low,
+    Medium,
+    Ok
+}
