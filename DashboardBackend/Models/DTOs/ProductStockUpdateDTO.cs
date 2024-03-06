1313657070d0be8b0300b8397fc30fd8a005cb43
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-03-06 08:42:53 +0600

    Update product DTO

---
+++ DashboardBackend/Models/DTOs/ProductStockUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DashboardBackend.Models.DTOs
+{
+    public class ProductStockUpdateDTO
+    {
+        public Category[] Categories { get; set; } = [];
+        public Size[] Sizes { get; set; } = [];
+        public Color[] Colors { get; set; } = [];
+        public int Stock { get; set; }
+        public int Price { get; set; }
+        public bool IsAdd { get; set; }
+    }
+}
