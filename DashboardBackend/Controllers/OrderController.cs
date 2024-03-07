536b38a6bb349b30ccae11466c4e0e611dc51cb8
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-03-07 08:53:44 +0600

    Add OrderController and GetAllOrders API

---
+++ DashboardBackend/Controllers/OrderController.cs
@@ -0,0 +1,69 @@
+﻿using DashboardBackend.Data;
+using DashboardBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DashboardBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationContext _db;
+        private APIResponse _response;
+
+        public OrderController(ApplicationContext db)
+        {
+            _db = db;
+            _response = new APIResponse();
+        }
+
+        // GET: api/<OrderController>
+        [HttpGet]
+        public async Task< ActionResult< APIResponse > > GetAllOrders() 
+        {
+            try
+            {
+                IEnumerable<Order> orders = await _db.Orders.ToListAsync();
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.IsSuccess = true;
+                _response.Message = "Successfully retrieved all orders";
+                _response.Data = orders;
+                return Ok(_response);
+            }
+            catch (Exception e)
+            {
+                _response.Message = e.Message;
+                return BadRequest(_response);
+            }
+        }
+
+        //// GET api/<OrderController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<OrderController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<OrderController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<OrderController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
