9170e07e4a619a992c98524e45607cb070bad146
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-03-07 08:46:31 +0600

    Add ID as Key

--- DashboardBackend/Models/Product.cs
+++ DashboardBackend/Models/Product.cs
@@ -5,6 +5,7 @@ namespace DashboardBackend.Models
     public class Product
     {
         [Key]
+        public int ID { get; set; }
         public string Name { get; set; } = string.Empty;
         public Category Category { get; set; }
         public Size Size { get; set; }
